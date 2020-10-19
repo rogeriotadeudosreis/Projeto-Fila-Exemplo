b2322926e23a54fa9623b370bb8b4bb39f228183
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-10-18 21:38:29 -0300

    Pequenas alterações

--- src/br/com/rogerio/app/Fila_Teste.java
+++ src/br/com/rogerio/app/Fila_Teste.java
@@ -10,20 +10,22 @@ import javax.swing.JOptionPane;
 public class Fila_Teste {
 
     public static void main(String[] args) {
-        Fila_Ex fila = new Fila_Ex(3);
+        Fila_Ex fila = new Fila_Ex(5);
 
         try {
             fila.enqueue(1);
             fila.enqueue(2);
             fila.enqueue(3);
-            fila.dequeue();
             fila.enqueue(4);
+            fila.enqueue(5);
             fila.dequeue();
             fila.dequeue();
-
-            System.out.println("Frente: " + fila.peek());
             fila.dequeue();
-            fila.dequeue();
+            System.out.println("Frente da Fila: " + fila.peek());
+            
+            
+            
+//            
 
         } catch (Exception erro) {
             System.out.println("\tAtenção!\n\t" +  erro.getMessage());
